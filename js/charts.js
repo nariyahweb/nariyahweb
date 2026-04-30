bf238e62067479221866e246bd8c398de912e7c2
nariyahweb <nariyahweb@gmail.com>
2026-04-30 11:13:10 +0700

    Create charts.js

---
+++ js/charts.js
@@ -0,0 +1,13 @@
+function loadChartCustomer(total, closing, pending, followup) {
+    if(chartCustomer) chartCustomer.destroy();
+    const ctx = document.getElementById("chartCustomer").getContext("2d");
+    
+    // ... kode chart customer ...
+}
+
+function loadChartProspek(data) {
+    if(chartProspek) chartProspek.destroy();
+    const ctx = document.getElementById("chartProspek").getContext("2d");
+    
+    // ... kode chart prospek ...
+}
