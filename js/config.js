522f061921502da20066d5c0824c73aca6ff3d46
nariyahweb <nariyahweb@gmail.com>
2026-04-30 11:07:41 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,20 @@
+// Konfigurasi Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyCfj2Xdj6et3fThyA2gg-GWG8yZOhoqREA",
+    authDomain: "floupyud.firebaseapp.com",
+    projectId: "floupyud"
+};
 
+// Inisialisasi Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Export variabel global
+const auth = firebase.auth();
+const db = firebase.firestore();
+const storage = firebase.storage();
+
+let currentCustomer = null;
+let currentRole = null;
+let importType = "prospek";
+let chartCustomer = null;
+let chartProspek = null;
